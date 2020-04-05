b3c039f4fd61e97d51b833c2b793c45d981ae911
NobleMathews <ebonleon@gmail.com>
2020-04-05 23:52:47 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -10,19 +10,42 @@
 //     };
 //   });
 // });
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
 $( document ).ready(function() {
 
-    if (!$.cookie("maCookie")) {
-        $('#cookie-banner').removeClass();
-        $('#cookie-banner').addClass('cookie-banner');
-        $('#cookie-banner').addClass('full');
-        $('#cookie-banner').addClass('bottom');
-        setTimeout(function(){
-          $('#cookie-banner').addClass('show');
-        }, 100);
-    }
-    $.cookie("maCookie", "Noble:Hi");
+  var user = getCookie("noble");
+  if (user == "") {
+    $('#cookie-banner').removeClass();
+    $('#cookie-banner').addClass('cookie-banner');
+    $('#cookie-banner').addClass('full');
+    $('#cookie-banner').addClass('bottom');
+    setTimeout(function(){
+      $('#cookie-banner').addClass('show');
+    }, 100);
+  } else {
+      setCookie("noble", "hi", 365);
+  }
     setTimeout(() => {   
         $(".tweeter").fadeIn(4000);
 }, 2500);
